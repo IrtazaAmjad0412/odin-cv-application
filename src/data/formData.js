6fd85f47b26f9c5da75023617e925c60f19bf4c4
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-10-28 11:10:51 -0400

    Added more fields to formData.js and renamed label for Full Name input to be Full Name

--- src/data/formData.js
+++ src/data/formData.js
@@ -1,10 +1,12 @@
 export const formData = [
-  { name: "fullName", label: "Name", type: "text", form: "personal" },
+  { name: "fullName", label: "Full Name", type: "text", form: "personal" },
   { name: "email", label: "Email", type: "email", form: "personal" },
   { name: "phoneNumber", label: "Phone Number", type: "tel", form: "personal" },
+  { name: "address", label: "Address", type: "text", form: "personal" },
   { name: "schoolName", label: "School", type: "text", form: "education" },
   { name: "titleOfStudy", label: "Title of Study", type: "text", form: "education" },
-  { name: "dateOfStudy", label: "Date of Study", type: "date", form: "education" },
+  { name: "startDate", label: "Start Date", type: "date", form: "education" },
+  { name: "endDate", label: "End Date", type: "date", form: "education" },
   { name: "companyName", label: "Company Name", type: "text", form: "experience" },
   { name: "positionTitle", label: "Position Title", type: "text", form: "experience" },
   {
